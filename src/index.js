39733e5866597dfcbca7e54161dc9e573cd1cd57
SalazarSDF <q37076111@gmail.com>
2022-06-22 19:36:35 +0300

    init: shelter grid layout

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//import "./index.html";
+//import "./our_pets.html";
+import "../src/styles/index.scss";
+// import "../src/js/makeCarusel.js";
+// import "../src/js/makeOurPetsCarusel.js";
+// import "../src/js/makeBurger.js";
+// import "../src/js/makePopUp.js";
+// import "../src/js/actPopUp.js";
+// import "../src/js/makeCardsPages.js";
---
+++ webpack.dev.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Set the mode to development or production
+  mode: "development",
+
+  entry: {
+    index: path.resolve(__dirname, "src/index.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js", // name == index, because of entry: key = index
+    clean: true, // можно убрать rm -r dist ??
+    assetModuleFilename: "[name][ext]", // это чтобы картинки сохраняли исходное название в dist
+  },
+
+  // Control how source maps are generated
+  // добавляет .map файл, который показывает в каком файле и на какой строке ошибка
+  devtool: "source-map",
+
+  // Spin up a server for quick development
+  devServer: {
+    compress: true,
+    hot: true,
+    port: 8080,
+
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      //title: "Shelter-main",
+      template: "src/index.html", // помогает найти html
+    }),
+    new HtmlWebpackPlugin({
+      filename: "our_pets.html",
+      //title: "Shelter-main",
+      template: "src/our_pets.html", // помогает найти html
+    }),
+  ],
+};
