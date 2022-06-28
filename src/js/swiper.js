5accab2f20a71f2d4fcdab15498f7b97d8c755a3
SalazarSDF <q37076111@gmail.com>
2022-06-28 17:38:46 +0300

    implement: responsive for tablet

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -20,17 +20,15 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
   loop: true,
   // Responsive breakpoints
-  // breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30,
-  //   },
-  //   // when window width is >= 640px
-  // },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1250: {
+      slidesPerView: 3,
+    },
+  },
 });
