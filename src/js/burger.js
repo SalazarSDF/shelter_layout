2d21ecee1a43289988aa48291a9a8c9d0e610881
SalazarSDF <q37076111@gmail.com>
2022-06-30 16:01:47 +0300

    fix: overflow when burger open

--- src/js/burger.js
+++ src/js/burger.js
@@ -3,12 +3,10 @@ let burgerIcon = document.querySelector(".burger-icon");
 let burgerWrapper = document.querySelector(".burger-icon__wrapper");
 let body = document.querySelector("body");
 
-
 function showBurger(e) {
   burger.classList.toggle("--active");
   burgerIcon.classList.toggle("--active");
   //overflow
-  body.classList.toggle("--active");
 }
 
 burgerWrapper.addEventListener("click", showBurger);
