dc3b4d13661afb7e8e0dd5fc24b3ecc686fd6e83
SalazarSDF <q37076111@gmail.com>
2022-06-30 13:46:51 +0300

    fix: popup layout on small brake point

--- src/js/burger.js
+++ src/js/burger.js
@@ -3,7 +3,6 @@ let burgerIcon = document.querySelector(".burger-icon");
 let burgerWrapper = document.querySelector(".burger-icon__wrapper");
 let body = document.querySelector("body");
 
-console.log(burgerIcon);
 
 function showBurger(e) {
   burger.classList.toggle("--active");
