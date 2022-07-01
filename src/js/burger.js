1e76ba067b47d5732cac06b0d07b10c5e06cab13
SalazarSDF <q37076111@gmail.com>
2022-07-01 05:37:28 +0300

    fix: burger

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,12 +1,22 @@
 let burger = document.querySelector(".burger");
 let burgerIcon = document.querySelector(".burger-icon");
 let burgerWrapper = document.querySelector(".burger-icon__wrapper");
-let body = document.querySelector("body");
 
-function showBurger(e) {
+function closeBurger(e) {
+  let burgerContant = document.querySelector(".burger__contant");
+  console.log(burgerContant);
+  if (e.target !== burgerContant) {
+    burger.classList.toggle("--active");
+    burgerIcon.classList.toggle("--active");
+  }
+}
+
+function showBurger() {
   burger.classList.toggle("--active");
   burgerIcon.classList.toggle("--active");
   //overflow
+  //
 }
 
 burgerWrapper.addEventListener("click", showBurger);
+burger.addEventListener("click", closeBurger);
